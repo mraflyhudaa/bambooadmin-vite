95aeff02f38622d0c579123237a36a71fad3a3a4
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2022-12-24 18:12:28 +0700

    changes total users & orders

--- src/components/FeaturedInfo.jsx
+++ src/components/FeaturedInfo.jsx
@@ -5,10 +5,13 @@ import { userRequest } from '../requestMethods';
 const FeaturedInfo = () => {
   const [income, setIncome] = useState([]);
   const [perc, setPerc] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
+  const [totalUsers, setTotalUsers] = useState([]);
+  const [totalOrders, setTotalOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getIncome = async () => {
+      setIsLoading(true);
       try {
         const res = await userRequest.get('orders/income');
         setIncome(res.data);
@@ -19,6 +22,36 @@ const FeaturedInfo = () => {
       }
     };
     getIncome();
+  }, []);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      setIsLoading(true);
+      try {
+        const res = await userRequest.get('users');
+        setTotalUsers(res.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
+    getUsers();
+  }, []);
+
+  useEffect(() => {
+    const getOrders = async () => {
+      setIsLoading(true);
+      try {
+        const res = await userRequest.get('orders');
+        setTotalOrders(res.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
+    getOrders();
   }, []);
 
   console.log(income);
@@ -35,7 +68,7 @@ const FeaturedInfo = () => {
       <div className='flex-1 mx-5 p-[30px] rounded-[10px] cursor-pointer shadow-[0px_0px_15px_-10px_rgba(0,0,0,0.75)]'>
         <span className='text-xl'>Revanue</span>
         <div className='my-[10px] flex items-center'>
-          <span className='text-3xl'>Rp. {income[1]?.total} </span>
+          <span className='text-3xl'>Rp. {income[1]?.total}</span>
           <span className='flex items-center ml-5'>
             %{Math.floor(perc)}
             {perc < 0 ? (
@@ -50,20 +83,18 @@ const FeaturedInfo = () => {
       <div className='flex-1 mx-5 p-[30px] rounded-[10px] cursor-pointer shadow-[0px_0px_15px_-10px_rgba(0,0,0,0.75)]'>
         <span className='text-xl'>Customer</span>
         <div className='my-[10px] flex items-center'>
-          <span className='text-3xl'>12</span>
+          <span className='text-3xl'>{totalUsers.length}</span>
           <span className='flex items-center ml-5'></span>
         </div>
-        <span className='text-base text-gray-900'>Compared to last month</span>
       </div>
       <div className='flex-1 mx-5 p-[30px] rounded-[10px] cursor-pointer shadow-[0px_0px_15px_-10px_rgba(0,0,0,0.75)]'>
         <span className='text-xl'>Transaction</span>
         <div className='my-[10px] flex items-center'>
-          <span className='text-3xl'>10</span>
+          <span className='text-3xl'>{totalOrders.length}</span>
           {/* <span className='flex items-center ml-5'>
             +2.4 <ArrowUpward className='text-sm' />
           </span> */}
         </div>
-        <span className='text-base text-gray-900'>Compared to last month</span>
       </div>
     </div>
   );
