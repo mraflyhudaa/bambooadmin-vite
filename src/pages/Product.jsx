b346d0a6e69e22921c9c7ed164b55ab9b7dd325b
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2022-12-24 18:10:17 +0700

    add firebaseStorage

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,6 +7,14 @@ import Input from '../components/Input';
 import Chart from '../components/Chart';
 import { PencilIcon } from '@heroicons/react/solid';
 import Spinner from '../components/Spinner';
+import { Publish } from '@mui/icons-material';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import app from '../firebaseConfig';
 
 const Product = () => {
   const location = useLocation();
@@ -19,6 +27,7 @@ const Product = () => {
   const [cat, setCat] = useState([]);
   const [dimension, setDimension] = useState([]);
   const [price, setPrice] = useState([]);
+  const [preview, setPreview] = useState('');
   const [isFetching, setIsFetching] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
   const dispatch = useDispatch();
@@ -42,6 +51,12 @@ const Product = () => {
     setPrice(e.target.value.split(','));
   };
 
+  const loadImage = (e) => {
+    const image = e.target.files[0];
+    setFile(image);
+    setPreview(URL.createObjectURL(image));
+  };
+
   useEffect(() => {
     const getProduct = async () => {
       setIsFetching(true);
@@ -49,6 +64,7 @@ const Product = () => {
         .get(`/products/find/${productId}`)
         .then((res) => {
           setProduct(res.data.data);
+          setFile(res.data.data.img);
           setIsFetching(false);
           console.log(res.data.data);
         })
@@ -62,18 +78,75 @@ const Product = () => {
 
   const handleClick = async () => {
     setIsUpdating(true);
-    await userRequest
-      .put(`products/${productId}`, {
-        inputs,
-        categories: cat.length == 0 ? product.categories : cat,
-        dimension: dimension.length == 0 ? product.dimension : dimension,
-        price: price.length == 0 ? product.price : price,
-      })
-      .then((res) => {
-        console.log(res.data);
-        toast.success(res.data.message);
-      })
-      .catch((err) => console.log(err));
+
+    if (file == product.img) {
+      await userRequest
+        .put(`products/${productId}`, {
+          ...inputs,
+          categories: cat.length == 0 ? product.categories : cat,
+          dimension: dimension.length == 0 ? product.dimension : dimension,
+          price: price.length == 0 ? product.price : price,
+        })
+        .then((res) => {
+          console.log(res.data);
+          toast.success(res.data.message);
+        })
+        .catch((err) => console.log(err));
+      console.log(file == null);
+    } else {
+      const fileName = new Date().getTime() + file.name;
+      const storage = getStorage(app);
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+            default:
+          }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+          console.log(error);
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            userRequest
+              .put(`products/${productId}`, {
+                ...inputs,
+                img: file == product.img ? product.img : downloadURL,
+                categories: cat.length == 0 ? product.categories : cat,
+                dimension:
+                  dimension.length == 0 ? product.dimension : dimension,
+                price: price.length == 0 ? product.price : price,
+              })
+              .then((res) => {
+                console.log(res.data);
+                toast.success(res.data.message);
+              })
+              .catch((err) => console.log(err));
+          });
+        }
+      );
+    }
   };
 
   const MONTHS = useMemo(
@@ -119,7 +192,7 @@ const Product = () => {
       <div className='flex items-center justify-between'>
         <h1 className='productTitle'>Product</h1>
         <Link to='/newproduct'>
-          <button className='w-20 border-none p-[5px] bg-green-600 text-white ronuded-[5px] text-base cursor-pointer'>
+          <button className='w-20 border-none p-[5px] bg-green-600 rounded-[5px] cursor-pointer text-white text-base'>
             Create
           </button>
         </Link>
@@ -210,13 +283,15 @@ const Product = () => {
                   name='inStock'
                   id='idStock'
                   defaultValue={product.inStock}
-                  onChange={handleChange}>
-                  <option value='true'>Yes</option>
-                  <option value='false'>No</option>
+                  onChange={handleChange}
+                >
+                  <option value={true}>Yes</option>
+                  <option value={false}>No</option>
                 </select>
                 <label
                   htmlFor='desc'
-                  className='block text-sm font-semibold text-black mt-4'>
+                  className='block text-sm font-semibold text-black mt-4'
+                >
                   Description
                 </label>
                 <textarea
@@ -227,12 +302,31 @@ const Product = () => {
                   className='form-textarea mt-2 appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-green-500 focus:border-green-500 green:z-10 sm:text-sm'
                 />
               </div>
+              <div className='flex flex-col justify-around'>
+                <div className='flex items-center mt-6'>
+                  <img
+                    src={preview ? preview : file}
+                    alt=''
+                    className='w-[100px] h-[100px] rounded-xl object-cover mr-5'
+                  />
+                  <label htmlFor='file'>
+                    <Publish className='cursor-pointer' />
+                  </label>
+                  <input
+                    type='file'
+                    id='file'
+                    style={{ display: 'none' }}
+                    onChange={loadImage}
+                  />
+                </div>
+              </div>
               <div className='flex flex-col justify-between'>
                 <button
                   type='button'
                   onClick={handleClick}
                   disabled={inputs == undefined ? true : false}
-                  className=' w-full flex justify-center py-2 px-4  border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 disabled:bg-gray-300  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mt-6'>
+                  className=' w-full flex justify-center py-2 px-4  border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 disabled:bg-gray-300  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mt-6'
+                >
                   <PencilIcon
                     className='h-5 w-5 text-green-500 group-hover:text-green-400 '
                     aria-hidden='true'
